8e076e9c3bfb57f555b692c8e729cabf9f6add2e
chinu3 <chinmaya@eka.care>
2022-09-06 22:27:45 +0530

    cleanups

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useLayoutEffect } from "react";
-import PortfolioContent from "../components/portfolio/portfolio-content";
-import PortfolioTopNav from "../components/portfolio/portfolio-top-nav";
-
 const Home = () => {
   useEffect(() => {
     console.log(1);
@@ -24,8 +21,8 @@ const Home = () => {
         <title>Chinmaya Bisoi | Frontend Developer and Enthusiast</title>
         <link rel="icon" href="/icons/bolt-lightning-solid.svg" />
       </Head>
-      <PortfolioTopNav />
-      <PortfolioContent />
+      {/* <PortfolioTopNav />
+      <PortfolioContent /> */}
     </div>
   );
 };
